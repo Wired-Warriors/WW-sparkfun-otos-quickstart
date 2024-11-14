bd20841146b3dc1e3590e72cd52069570a05670f
a-krier <kriergenics@gmail.com>
2024-11-14 15:50:30 -0500

    Added initial opmode with OTOS telemetry, updated beginPose, and copied in SplineTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TEST_AUTO_Development.java
@@ -0,0 +1,142 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.SparkFunOTOSDrive;
+
+import org.firstinspires.ftc.robotcore.internal.camera.delegating.DelegatingCaptureSequence;
+
+import java.lang.Math;
+
+/*
+ * This is our development OpMode for AUTO, the first one we're experimenting with in java for 23168
+ * We will integrate to following in this mode:
+ *    - Limelight3a (limelight)
+ *    - Sparkfun OTOS (sensor_otos)
+ *    - Mecanum drive (motors left_front, left_back, right_front, right_back)
+ *    - Road Runner using a localizer for the OTOS developed by @j5155 on the FTC Discord https://github.com/jdhs-ftc
+ *
+ * All of the drive configuration is done via MecanumDrive.java, you do not have to manage that here.
+ *
+ * THIS MODE IS CONFIGURED FOR PANCAKE, NOT WAFFLES
+ *
+ */
+@Autonomous(name="TEST_AUTO_Development", group="AUTO")
+@Disabled
+public class TEST_AUTO_Development extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private Limelight3A limelight;
+    private SparkFunOTOS otos;
+
+    // Declare variables
+    //TODO ********** Set the Alliance Color  **************
+    public String colorAlliance = "BLUE"; //Enter either BLUE or RED alliance, this will chance all further color-related settings
+    public double txLimelight;
+    public double tyLimelight;
+
+    //TODO *********** Set the starting pose for the robot based on the alliance start position, X and Y in INCHES from the center of the field, heading in RADIANS
+    Pose2d beginPose = new Pose2d(30, 30, Math.PI/2);
+
+    @Override
+    public void runOpMode() {
+        initSensors();
+        // Set initial limelight pipeline for alliance color: 0=red, 1=blue, 2=yellow
+        if (colorAlliance == "BLUE") {
+            limelight.pipelineSwitch(1);
+        }
+        else {
+            limelight.pipelineSwitch(0);
+        }
+
+        //Instantiate the roadrunner mechanum drive (via the OTOS localizer)
+        SparkFunOTOSDrive drive = new SparkFunOTOSDrive(hardwareMap, beginPose);
+
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        ////////////////////////////////////////////////////////////////////////////////////
+        // Wait for the game to start (driver presses START)
+        ////////////////////////////////////////////////////////////////////////////////////
+        waitForStart();
+
+        //Create the first drive path
+        Actions.runBlocking(
+                drive.actionBuilder(beginPose)
+                        .splineTo(new Vector2d(60, 60), Math.PI / 2)
+                        .splineTo(new Vector2d(0, 60), Math.PI)
+                        .build());
+
+
+
+        SparkFunOTOS.Pose2D pos = otos.getPosition(); //Read OTOS Pose for telemetry
+
+        LLResult result = limelight.getLatestResult();
+        if (result != null && result.isValid()) {
+            double tx = result.getTx(); // How far left or right the target is (degrees)
+            double ty = result.getTy(); // How far up or down the target is (degrees)
+            double ta = result.getTa(); // How big the target looks (0%-100% of the image)
+
+            telemetry.addData("Target X", tx);
+            telemetry.addData("Target Y", ty);
+            telemetry.addData("Target Area", ta);
+        } else {
+            telemetry.addData("Limelight", "No Targets");
+        }
+
+        telemetry.addLine();
+        telemetry.addData("OTOS Data", "X: (%.1f), Y: (%.1f), H: (%.2f)", pos.x,pos.y,pos.h);
+        telemetry.update();
+
+        // run until the end of the match (driver presses STOP)
+    }
+    private void initSensors(){
+        //INIT LIMELIGHT
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        telemetry.setMsTransmissionInterval(11);
+        limelight.pipelineSwitch(0); // Set a default defined pipeline
+        limelight.start(); // This tells Limelight to start looking!
+    }
+}
